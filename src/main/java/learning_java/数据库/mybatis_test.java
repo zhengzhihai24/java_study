519d33c8a214df115eda598602b58c72cb602af6
zhengzhihai24 <zhengzhihai24@gmail.com>
2025-03-19 01:51:37 +0800

    mybatis系列,查询练习

--- src/main/java/learning_java/数据库/mybatis_test.java
+++ src/main/java/learning_java/数据库/mybatis_test.java
@@ -1,5 +1,9 @@
 package learning_java.数据库;
 
+import learning_java.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.apache.ibatis.cursor.Cursor;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
@@ -7,6 +11,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Map;
 
 public class mybatis_test {
     public static void main(String[] args) throws FileNotFoundException {
@@ -15,7 +20,37 @@ public class mybatis_test {
         try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
             List<User> users = sqlSession.selectList("selectAllUser");
             users.forEach(System.out::println);
+            User user = sqlSession.selectOne("selectUserById", 1);
+            System.out.println(user);
+            System.out.println("测试多查询");
+            List<User> user1 = sqlSession.selectList("selectUserByAgeandgender", Map.of("age",22,"gender","Male"));
+            user1.forEach(System.out::println);
+            System.out.println("实体类模式查询");
+            List<User> user2 = sqlSession.selectList("selectUserByAgeandgender",new Param(22,"Male"));
+            user2.forEach(System.out::println);
+            System.out.println("条件查询");
+            List<User> user3 = sqlSession.selectList("selectUserAgeOver",18);
+            user3.forEach(System.out::println);
+            System.out.println("Map查询");
+            Map<String,User> map = sqlSession.selectMap("selectUserAgeOver",18 ,"name");
+            System.out.println(map);
+            System.out.println("游标cursor查询");
+            Cursor<User> cursor = sqlSession.selectCursor("selectUserAgeOver", 18);
+//            cursor.forEach(System.out::println);
+            for(User usercursor : cursor){
+                System.out.println(usercursor);
+                System.out.println(cursor.getCurrentIndex());
+                System.out.println(usercursor.getName());
+            }
+
+
         }
 
+        }
+        @Data
+        @AllArgsConstructor
+        static class Param{
+         int age;
+         String gender;
     }
 }
