028b29d0754e22a0fd2d4824c4fe46d472bc593a
zhengzhihai24 <zhengzhihai24@gmail.com>
2025-03-19 01:51:37 +0800

    mybatis系列,查询练习

--- src/main/java/learning_java/entity/User.java
+++ src/main/java/learning_java/entity/User.java
@@ -1,10 +1,14 @@
-package learning_java.数据库;
+package learning_java.entity;
 
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public class User {
     int id ;
     String name ;
     int age ;
+    String email ;
+    Date createdAt ;
 }
